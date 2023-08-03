a086b7a69f2c33b555c4c77e3241c09b7529a2e1
okewunmi <okewunmiafeezolaide@gmail.com>
2023-08-03 14:01:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <df-messenger
+        intent="WELCOME"
+        chat-title="eDokita"
+        agent-id="aeea0270-e438-4c0f-9ab8-6a2c4964458f"
+        language-code="en"
+      ></df-messenger>
     </div>
   );
 }
