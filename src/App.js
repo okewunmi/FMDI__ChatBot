5180efea7219684be43fd3cab6e6ddc4fb627b0c
okewunmi <okewunmiafeezolaide@gmail.com>
2023-08-31 15:50:45 +0100

    commit state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
     <div className="main">
       <div className="header"></div>
       <df-messenger
-        intent="WELCOME"
-        chat-title="GlobalNet Nexus"
-        agent-id="be6fb2e4-16e2-43de-a079-35fcbb417d9c"
-        language-code="en"
-      ></df-messenger>
+  intent="WELCOME"
+  chat-title="eDokita"
+  agent-id="aeea0270-e438-4c0f-9ab8-6a2c4964458f"
+  language-code="en"
+></df-messenger>
     </div>
   );
 }
